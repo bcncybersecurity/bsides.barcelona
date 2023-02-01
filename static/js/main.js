8badbd47f970e76a27b4fd7f6ba3c3c9dc02a9e8
Davide Barbato <anathema@anche.no>
2023-02-01 20:43:39 +0100

    New BSidesBCN23 website

--- static/js/main.js
+++ static/js/main.js
@@ -1,278 +1,105 @@
-/* =================================
-------------------------------------
-	Labs - Design Studio
-	Version: 1.0
- ------------------------------------ 
- ====================================*/
+/*
+Lektor-Icon Theme
+Copyright (c) 2016- Lektor-Icon Contributors
 
-'use strict';
+Original standalone HTML5 theme distributed under the terms of the
+Creative Commons Attribution 3.0 license -->
+https://creativecommons.org/licenses/by/3.0/
 
-/*------------------
-	Preloder
---------------------*/
-function loader() {
-	$(window).on('load', function() { 
-		$(".loader").fadeOut(); 
-		$("#preloder").delay(400).fadeOut("slow");
-	});
-}
+Additions, modifications and porting released under the terms of the
+MIT (Expat) License: https://opensource.org/licenses/MIT
+See the LICENSE.txt file for more details
+https://github.com/spyder-ide/lektor-icon/blob/master/LICENSE.txt
 
+For information on the included third-party assets, see NOTICE.txt
+https://github.com/spyder-ide/lektor-icon/blob/master/NOTICE.txt
+*/
 
+;(function () {
+  'use strict'
 
-/*------------------
-	Navigation
---------------------*/
-function responsive() {
-	// Responsive 
-	$('.responsive').on('click', function(event) {
-		$('.menu-list').slideToggle(400);
-		event.preventDefault();
-	});
-}
+  // Fullsize Error Page Background
+  var fullHeight = function () {
+    var heightToSet = $(window).height() - $('.js-sticky').height() - $('#fh5co-footer').outerHeight()
+    $('#error-page').css('height', heightToSet)
+  }
 
+  var setFullHeight = function () {
+    fullHeight()
+    $(window).on('resize', fullHeight)
+  }
 
+  // Offcanvas layout for "hamburger" mobile menu
+  var offcanvasMenu = function () {
+    $('body').prepend('<div id="fh5co-offcanvas"></div>')
+    $('body').prepend('<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" aria-label="Toggle for hamburger menu"><i></i></a>')
 
-/*------------------
-	Hero Section
---------------------*/
-function heroSection() {
-	//Slide item bg image.
-	$('.hero-item').each(function() {
-		var image = $(this).data('bg');
-		$(this).css({
-			'background-image'  : 'url(' + image + ')',
-			'background-size'   : 'cover',
-			'background-repeat' : 'no-repeat',
-			'background-position': 'center bottom'
-		});
-	});
-	//slider auto height 
-	var iit = setInterval(slide_item, 1);
+    $('.fh5co-main-nav .fh5co-menu-1 a, .fh5co-main-nav .fh5co-menu-2 a').each(function () {
+      var $this = $(this)
+      $('#fh5co-offcanvas').append($this.clone())
+    })
+    // $('#fh5co-offcanvas').append
+  }
 
-	function slide_item() {
-		var bh = $('body').height();
-		$('.hero-item').height(bh);
-	}
-	slide_item();
+  // Top navbar stickiness
+  var mainMenuSticky = function () {
+    var sticky = $('.js-sticky')
+    var $section = $('.fh5co-main-nav')
 
-	var time = 7;
-	var $progressBar,
-		$bar, 
-		$elem, 
-		isPause, 
-		tick,
-		percentTime;
+    sticky.css('height', $section.height())
+    $(window).on('resize', function () {
+      sticky.css('height', $section.height())
+    })
 
-	// Init the carousel
-	$('#hero-slider').owlCarousel({
-		loop: true,
-		nav: true,
-		items: 1,
-		autoHeight:true,
-		animateOut: 'fadeOut',
-		animateIn: 'fadeIn',
-		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-		onInitialized: progressBar,
-		onTranslated: moved,
-		onDrag: pauseOnDragging
-	});
+    $section.waypoint(function (direction) {
+      if (direction === 'down') {
+        $section.css({
+          position: 'fixed',
+          top: 0,
+          width: '100%',
+          'z-index': 99999
+        }).addClass('fh5co-shadow')
+      }
+    }, {
+      offset: '0px'
+    })
+  }
 
-	// Init progressBar where elem is $("#owl-demo")
-	function progressBar(){    
-		// build progress bar elements
-		buildProgressBar();
+  // Mobile "burger" menu
+  var burgerMenu = function () {
+    $('body').on('click', '.js-fh5co-nav-toggle', function (event) {
+      var $this = $(this)
 
-		// start counting
-		start();
-	}
+      if ($('body').hasClass('offcanvas-visible')) {
+        $('body').removeClass('offcanvas-visible fh5co-overflow')
+        $this.removeClass('active')
+      } else {
+        $('body').addClass('offcanvas-visible fh5co-overflow')
+        $this.addClass('active')
+      }
+      event.preventDefault()
+    })
+  }
 
-	// create div#progressBar and div#bar then prepend to $("#owl-demo")
-	function buildProgressBar(){
-		$progressBar = $("<div>",{
-			id:"progressBar"
-		});
-		$bar = $("<div>",{
-			id:"bar"
-		});
-		$progressBar.append($bar).prependTo($("#hero-slider"));
-	}
+  // Click outside of offcanvas sidebar to close it
+  var mobileMenuOutsideClick = function () {
+    $(document).on('click', function (e) {
+      var container = $('#fh5co-offcanvas, .js-fh5co-nav-toggle')
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($('body').hasClass('offcanvas-visible')) {
+          $('body').removeClass('offcanvas-visible')
+          $('.js-fh5co-nav-toggle').removeClass('active')
+        }
+      }
+    })
+  }
 
-	function start() {
-		// reset timer
-		percentTime = 0;
-		isPause = false;
-		// run interval every 0.01 second
-		tick = setInterval(interval, 10);
-	};
-
-	function interval() {
-		if(isPause === false){
-			percentTime += 1 / time;
-
-			$bar.css({
-				width: percentTime+"%"
-			});
-
-			// if percentTime is equal or greater than 100
-			if(percentTime >= 100){
-				// slide to next item 
-				$("#hero-slider").trigger("next.owl.carousel");
-				percentTime = 0; // give the carousel at least the animation time ;)
-			}
-		}
-	}
-
-	// pause while dragging 
-	function pauseOnDragging(){
-		isPause = true;
-	}
-
-	// moved callback
-	function moved(){
-		// clear interval
-		clearTimeout(tick);
-		// start again
-		start();
-	}
-
-}
-
-
-
-/*------------------
-	Video Popup
---------------------*/
-function videoPopup() {
-	$('.video-popup').magnificPopup({
-		type: 'iframe',
-		autoplay : true
-	});
-}
-
-/*------------------
-	Agenda Popups
---------------------*/
-function agendasPopup() {
-    $('.open-agenda-popup').magnificPopup({
-        type: 'inline',
-        midClick: true
-    });
-}
-
-
-/*------------------
-	Testimonial
---------------------*/
-function testimonial() {
-	// testimonial Carousel 
-	$('#testimonial-slide').owlCarousel({
-		loop:true,
-		autoplay:true,
-		margin:30,
-		nav:false,
-		dots: true,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-				items:2
-			},
-			800:{
-				items:2
-			},
-			1000:{
-				items:2
-			}
-		}
-	});
-}
-
-
-
-/*------------------
-	Progress bar
---------------------*/
-function progressbar() {
-
-	$('.progress-bar-style').each(function() {
-		var progress = $(this).data("progress");
-		var prog_width = progress + '%';
-		if (progress <= 100) {
-			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
-		}
-		else {
-			$(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
-		}
-	});
-}
-
-
-
-/*------------------
-	Accordions
---------------------*/
-function accordions() {
-	$('.panel').on('click', function (e) {
-		$('.panel').removeClass('active');
-		var $this = $(this);
-		if (!$this.hasClass('active')) {
-			$this.addClass('active');
-		}
-		e.preventDefault();
-	});
-}
-
-
-
-/*------------------
-	Progress Circle
---------------------*/
-function progressCircle() {
-	//Set progress circle 1
-	$("#progress1").circleProgress({
-		value: 0.75,
-		size: 175,
-		thickness: 5,
-		fill: "#2be6ab",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 2
-	$("#progress2").circleProgress({
-		value: 0.83,
-		size: 175,
-		thickness: 5,
-		fill: "#2be6ab",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 3
-	$("#progress3").circleProgress({
-		value: 0.25,
-		size: 175,
-		thickness: 5,
-		fill: "#2be6ab",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 4
-	$("#progress4").circleProgress({
-		value: 0.95,
-		size: 175,
-		thickness: 5,
-		fill: "#2be6ab",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-}
-
-(function($) {
-	// Call all functions
-	loader();
-	responsive();
-	heroSection();
-	testimonial();
-	progressbar();
-    agendasPopup();
-	accordions();
-	progressCircle();
-
-})(jQuery);
+  // Document on DOM ready
+  $(function () {
+    offcanvasMenu()
+    burgerMenu()
+    mobileMenuOutsideClick()
+    mainMenuSticky()
+    setFullHeight()
+  })
+}())
